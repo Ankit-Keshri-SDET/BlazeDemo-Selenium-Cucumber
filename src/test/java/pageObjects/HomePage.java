c3356050ec26e392a92178ac22cf36e1683c56d6
Ankit-Keshri-QA <ankitkeshri198@gmail.com>
2025-04-25 08:23:21 +0530

    Started Building Page Objects for Website - 19

---
+++ src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,32 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage{
+
+    @FindBy(xpath="//a[@href='index.php']")
+    private WebElement logoLink;
+    @FindBy(xpath="//a[@href='home']")
+    private WebElement loginPageLink;
+    @FindBy(tagName="h1")
+    private WebElement headerText;
+    @FindBy(xpath="//p[contains(text(),'Blaze')]")
+    private WebElement firstText;
+    @FindBy(css="a[href*='vacation']")
+    private WebElement destinationLink;
+    @FindBy(xpath="//a[@href='index.php']")
+    private WebElement logoLink;
+    @FindBy(xpath="//a[@href='index.php']")
+    private WebElement logoLink;
+    @FindBy(xpath="//a[@href='index.php']")
+    private WebElement logoLink;
+
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
