58f054548b57e7833e73400bf73e775cb0926ddd
Ankit-Keshri-QA <ankitkeshri198@gmail.com>
2025-04-27 17:21:48 +0530

    Section 20 Done

--- src/test/java/runner/MyTestNGRunnerTest.java
+++ src/test/java/runner/MyTestNGRunnerTest.java
@@ -4,8 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/E2E_DataDriven.feature",
-        glue = {"stepDefinitions", "hooks", "factory","customeType","domainObjects"},
+        features = {"src/test/resources/features/E2E_DataDriven.feature",
+                    "src/test/resources/features/E2E_Flow.feature"},
+        glue = {"stepDefinitions", "hooks", "factory", "customeType", "domainObjects"},
         plugin = {"html:target/cucumber-reports/cucumber.html"
                 , "json:target/cucumber-dry.json", "pretty:target/cucumber-pretty-dry.txt"},
         monochrome = true
