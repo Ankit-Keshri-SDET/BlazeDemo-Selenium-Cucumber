6920a33716179334b62e4a021f8a5b5b2586b98a
Ankit-Keshri-QA <ankitkeshri198@gmail.com>
2025-05-01 17:30:20 +0530

    Run Pipeline agin

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -3,19 +3,28 @@ package factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import utils.ConfigLoader;
+
 import java.time.Duration;
 
 public class DriverFactory {
 
     public static WebDriver initializeDriver(String browser) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Use --headless=new for Chrome 109+
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
         WebDriver driver;
         switch (browser) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                driver = new ChromeDriver(options);
                 break;
             case "edge":
                 WebDriverManager.edgedriver().setup();
